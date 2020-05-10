369918efe684efca1a46272f0267239c135dd8a6
beingbiplov <beingbiplov@gmail.com>
2020-05-10 17:49:09 +0545

    about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavigationBar from './components/Navbar'
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   return (
     <div >
     <NavigationBar />
     <Header />
+    <About />
 
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './css/About.css'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import about_image from './assets/about_image.jpg'
+
+const About = () =>{
+	return(
+		<Container className='about'>
+			<h3 className='about_title'>
+					About Me
+				</h3>
+			<Row>
+				<Col md={7}>
+					<p>
+					  Hello! I'm Biplov, a fullstack developer based in Kathmandu, Nepal.
+					</p>
+					<p>
+					  I enjoy building things for the web, mostly with Django and JavaScript, but picking up
+					  a new language or framework is not a problem. 
+					  
+					</p>
+					<p>
+						I am a Computer Science student with diverse set of skills, ranging
+						from HTML/CSS + JavaScript, all the way to Django and React, and I am
+						comfortable developing on the backend as wll as frontend.
+					</p>
+				</Col>
+				<Col md={5}>
+					<div className='about_section_image'>
+						<Image className='about_image' src={about_image} rounded />
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	)
+
+}
+export default About
