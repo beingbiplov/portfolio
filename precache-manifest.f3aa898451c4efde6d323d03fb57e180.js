d1fd5a319844ec01d606d5d822313c3a76f74b55
beingbiplov <beingbiplov@gmail.com>
2020-05-13 17:48:10 +0545

    Updates

---
+++ precache-manifest.f3aa898451c4efde6d323d03fb57e180.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cda02b4ce644b5e3a2c2186d21631ff",
+    "url": "/beingbiplov.github.io/index.html"
+  },
+  {
+    "revision": "915df389af3ef4e87412",
+    "url": "/beingbiplov.github.io/static/css/main.24032b92.chunk.css"
+  },
+  {
+    "revision": "8d8039d90adb6cb7dee8",
+    "url": "/beingbiplov.github.io/static/js/2.39a7d7a4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/beingbiplov.github.io/static/js/2.39a7d7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915df389af3ef4e87412",
+    "url": "/beingbiplov.github.io/static/js/main.377bafc5.chunk.js"
+  },
+  {
+    "revision": "4a45e812d43f22f4e725",
+    "url": "/beingbiplov.github.io/static/js/runtime-main.f00b7119.js"
+  },
+  {
+    "revision": "12fdbca6b62d1ba3c66c5d13143cf28a",
+    "url": "/beingbiplov.github.io/static/media/about_image.12fdbca6.jpg"
+  },
+  {
+    "revision": "30d8879f6268e2a2bcdd011e6ab24996",
+    "url": "/beingbiplov.github.io/static/media/bootstrap.30d8879f.svg"
+  },
+  {
+    "revision": "136863b9aa29fc857a81f9d385a16bd0",
+    "url": "/beingbiplov.github.io/static/media/django.136863b9.svg"
+  },
+  {
+    "revision": "d2326c3fd6fedeb167744949466bbaf9",
+    "url": "/beingbiplov.github.io/static/media/django1.d2326c3f.svg"
+  },
+  {
+    "revision": "f6050941083c77b1ec0b6753a1456dfa",
+    "url": "/beingbiplov.github.io/static/media/git.f6050941.svg"
+  },
+  {
+    "revision": "b0cbe41d5658fdacc6b6d9b1447cc35d",
+    "url": "/beingbiplov.github.io/static/media/htmlcss.b0cbe41d.png"
+  },
+  {
+    "revision": "2f21395dc0878c5b80138269d6acbda1",
+    "url": "/beingbiplov.github.io/static/media/javascript.2f21395d.svg"
+  },
+  {
+    "revision": "a903a0ac018ea485f34ce7000346cf75",
+    "url": "/beingbiplov.github.io/static/media/postalcodes_nepal.a903a0ac.png"
+  },
+  {
+    "revision": "01766e9cdd6e12e020a2c9ca505fd961",
+    "url": "/beingbiplov.github.io/static/media/python.01766e9c.svg"
+  },
+  {
+    "revision": "1f6a76a4bf66ee1d6bdeab1edc5e2b5c",
+    "url": "/beingbiplov.github.io/static/media/react.1f6a76a4.svg"
+  },
+  {
+    "revision": "5e2aac4c136094c038da47714858112e",
+    "url": "/beingbiplov.github.io/static/media/sampadak.5e2aac4c.png"
+  },
+  {
+    "revision": "b8e1ad87714e4f1acf5f84b778395c2b",
+    "url": "/beingbiplov.github.io/static/media/weatherapp.b8e1ad87.png"
+  }
+]);
